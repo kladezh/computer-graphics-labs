382b1e2daa9d49b1507392cad2da2992c432cb6c
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-09-14 20:17:10 +0300

    Realize drawing line method by Bresenham's line algorithm.

--- SEM5-LR1/Form.cs
+++ SEM5-LR1/Form.cs
@@ -77,7 +77,61 @@ namespace SEM5_LR1
 
         public void DrawLine(Point first, Point second)
         {
-            // draw by brezenheim algorithm
+            DrawLineBresenham(first, second);
+        }
+        private void DrawLineBresenham(Point first, Point second)
+        {
+            /* Прим. 
+             * Алгоритм не работает для крутых отрезков
+             * (т.е. угол наклона относительно OX больше 45 гр.)
+             * и начальная точка считается та, которая левее
+             * поэтому приходится точки "менять местами"
+             */
+
+            int x1 = first.X, y1 = first.Y;
+            int x2 = second.X, y2 = second.Y;
+
+            // проверка отрезка на крутость
+            bool isSteep = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+
+            // отражаем по диагонали, если крутой
+            if(isSteep)
+            {
+                Swapper.Swap(ref x1, ref y1);
+                Swapper.Swap(ref x2, ref y2);
+            }
+
+            // меняем местами точки, если отрезок растёт справа налево
+            if(x1 > x2)
+            {
+                Swapper.Swap(ref x1, ref x2);
+                Swapper.Swap(ref y1, ref y2);
+            }
+
+            int dx = x2 - x1;
+            int dy = Math.Abs(y2 - y1);
+
+            int error = dx / 2;
+
+            int ystep = (y1 < y2) ? 1 : -1;
+
+            int y = y1;
+            for (int x = x1; x <= x2; x++)
+            {
+                Graphics.FillRectangle(
+                    Pen.Brush, 
+                    x: isSteep ? y : x, 
+                    y: isSteep ? x : y, 
+                    width: 1, 
+                    height: 1);
+
+                error -= dy;
+                if(error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
         }
 
         public void AddPoint(Point point)
@@ -90,6 +144,17 @@ namespace SEM5_LR1
                 return;
 
             DrawLine(Points[Points.Count() - 1], Points[Points.Count() - 2]);
+        }
+    }
+
+    public class Swapper
+    {
+        static public void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
         }
     }
 }
