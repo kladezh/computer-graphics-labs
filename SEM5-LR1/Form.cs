aa420295777036fec4d6dd624d1fc450edc8a48a
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-09-14 16:39:47 +0300

    Add CurvePainter class that will be draw points and lines on pictureBox.

--- SEM5-LR1/Form.cs
+++ SEM5-LR1/Form.cs
@@ -12,18 +12,53 @@ namespace SEM5_LR1
 {
     public partial class Form : System.Windows.Forms.Form
     {
+        private CurvePainter painter;
+
         public Form()
         {
             InitializeComponent();
+
+            painter = new CurvePainter
+            {
+                PaintContext = pictureBox
+            };
         }
        
 
         private void buttonClear_Click(object sender, EventArgs e)
         {
+            painter.Clear();
         }
 
         private void pictureBox_MouseUp(object sender, MouseEventArgs e)
         {
         }
+    }
+
+    public class CurvePainter
+    {
+        private Graphics Graphics { get; set; }
+        private List<Point> Points { get; set; }
+
+        public Control PaintContext
+        {
+            get => PaintContext;
+            set 
+            {
+                PaintContext = value;
+                Graphics = value.CreateGraphics();
+            }
+        }
+
+        public CurvePainter()
+        {
+            Points = new List<Point>();
+        }
+
+        public void Clear()
+        {
+            Graphics.Clear(PaintContext.BackColor);
+        }
+
     }
 }
