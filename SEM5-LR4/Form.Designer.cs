74ec107206d35b7f63becd8f87dd20d3a4cf04fe
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-11-03 17:54:59 +0300

    Add app template and basic design for lab 4.

---
+++ SEM5-LR4/Form.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SEM5_LR4
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.panelPainter = new System.Windows.Forms.Panel();
+            this.buttonClip = new System.Windows.Forms.Button();
+            this.radioButtonCurve = new System.Windows.Forms.RadioButton();
+            this.radioButtonPolygon = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonDrawPolygon = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panelPainter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(776, 379);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(711, 403);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // panelPainter
+            // 
+            this.panelPainter.Controls.Add(this.radioButtonPolygon);
+            this.panelPainter.Controls.Add(this.radioButtonCurve);
+            this.panelPainter.Location = new System.Drawing.Point(42, 398);
+            this.panelPainter.Name = "panelPainter";
+            this.panelPainter.Size = new System.Drawing.Size(158, 28);
+            this.panelPainter.TabIndex = 2;
+            // 
+            // buttonClip
+            // 
+            this.buttonClip.Location = new System.Drawing.Point(630, 403);
+            this.buttonClip.Name = "buttonClip";
+            this.buttonClip.Size = new System.Drawing.Size(75, 23);
+            this.buttonClip.TabIndex = 3;
+            this.buttonClip.Text = "clip";
+            this.buttonClip.UseVisualStyleBackColor = true;
+            this.buttonClip.Click += new System.EventHandler(this.buttonClip_Click);
+            // 
+            // radioButtonCurve
+            // 
+            this.radioButtonCurve.AutoSize = true;
+            this.radioButtonCurve.Location = new System.Drawing.Point(3, 6);
+            this.radioButtonCurve.Name = "radioButtonCurve";
+            this.radioButtonCurve.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonCurve.TabIndex = 0;
+            this.radioButtonCurve.TabStop = true;
+            this.radioButtonCurve.Text = "curve";
+            this.radioButtonCurve.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonPolygon
+            // 
+            this.radioButtonPolygon.AutoSize = true;
+            this.radioButtonPolygon.Location = new System.Drawing.Point(93, 6);
+            this.radioButtonPolygon.Name = "radioButtonPolygon";
+            this.radioButtonPolygon.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonPolygon.TabIndex = 1;
+            this.radioButtonPolygon.TabStop = true;
+            this.radioButtonPolygon.Text = "polygon";
+            this.radioButtonPolygon.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(206, 406);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // buttonDrawPolygon
+            // 
+            this.buttonDrawPolygon.Location = new System.Drawing.Point(166, 432);
+            this.buttonDrawPolygon.Name = "buttonDrawPolygon";
+            this.buttonDrawPolygon.Size = new System.Drawing.Size(75, 23);
+            this.buttonDrawPolygon.TabIndex = 6;
+            this.buttonDrawPolygon.Text = "draw";
+            this.buttonDrawPolygon.UseVisualStyleBackColor = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 466);
+            this.Controls.Add(this.buttonDrawPolygon);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.buttonClip);
+            this.Controls.Add(this.panelPainter);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form";
+            this.Text = "Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panelPainter.ResumeLayout(false);
+            this.panelPainter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Panel panelPainter;
+        private System.Windows.Forms.RadioButton radioButtonCurve;
+        private System.Windows.Forms.Button buttonClip;
+        private System.Windows.Forms.RadioButton radioButtonPolygon;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonDrawPolygon;
+    }
+}
+
---
+++ SEM5-LR4/Form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEM5_LR4
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClip_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
