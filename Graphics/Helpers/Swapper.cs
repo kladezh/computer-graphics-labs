bcfe890e76923c843fd5ffef94378e4cfaf09600
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-12-15 23:57:47 +0300

    Add class library for general Painter class.

---
+++ Graphics/Helpers/Swapper.cs
@@ -0,0 +1,13 @@
+﻿namespace Graphics.Helpers
+{
+    public class Swapper
+    {
+        static public void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+    }
+}
