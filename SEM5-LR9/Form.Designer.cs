6182fa194a4c15c32f603ff29bd86301a36874a1
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2022-12-23 00:39:27 +0300

    lab9: Add app template for drawing set of Julia.

---
+++ SEM5-LR9/Form.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace SEM5_LR9
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(821, 589);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 619);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ SEM5-LR9/Form.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Graphics.Services;
+
+namespace SEM5_LR9
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private Bitmap _bitmap;
+        private Painter _painter;
+
+        public Form()
+        {
+            InitializeComponent();
+
+            _bitmap = new Bitmap(pictureBox.ClientSize.Width, pictureBox.ClientSize.Height);
+            pictureBox.DrawToBitmap(_bitmap, pictureBox.ClientRectangle);
+
+            _painter = new Painter
+            {
+                Context = _bitmap,
+                Pen = new Pen(Color.Black, 2f),
+            };
+
+            DrawJuliaSet();
+        }
+
+        private void DrawJuliaSet()
+        {
+            // code...
+        }
+
+        private void Form_Paint(object sender, PaintEventArgs e)
+        {
+            pictureBox.Image = _bitmap;
+        }
+    }
+}
